ec7ebe29d970705891b2535da270ea5247a17b95
Aditya07024 <adityakumar07024@gmail.com>
2026-05-01 20:06:05 +0530

    deploymenty

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,5 +1,7 @@
+/Users/aditya/Desktop/startup/client/next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   experimental: {
     typedRoutes: false,
   },
